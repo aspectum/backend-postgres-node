0f5b4d206d2af0aa51ed41860f6840a22abf19aa
Pedro Garcia <drogpe@gmail.com>
2021-01-21 13:47:06 -0300

    Connected to db

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const knex = require('knex')
 const app = express();
 const port = 3000
 
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : 'localhost',
+      user : 'postgres',
+      password : 'root',    // Should I use env variables or it doesn't matter because it's the backend?
+      database : 'db'
+    }
+});
+
 app.get('/', (req, res) => {
-    res.send('it works');
+    db.select('*').from('usuarios')
+        .then(data => {
+            res.json(data)
+        })
 })
 
 app.listen(port, () => {
