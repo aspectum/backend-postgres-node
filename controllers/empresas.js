fb8050265f55e3cb775fbeb7451238c88f1b6d22
Pedro Garcia <drogpe@gmail.com>
2021-01-22 15:46:46 -0300

    PUT /empresas/:id endpoint implemented

--- controllers/empresas.js
+++ controllers/empresas.js
@@ -1,4 +1,5 @@
 // Middleware to validate request
+// How to validate PUT?
 const validateRequest = (type) => (req, res, next) => {
     let isValid = true;
 
@@ -74,8 +75,59 @@ const create = (db) => (req, res) => {
         })
 }
 
+
+// Edits empresa
+const update = (db) => (req, res) => {
+    const { id } = req.params;
+    const { slug, razao_social, email } = req.body;
+    const logged_user_id = req.authData.id;
+
+    const updated_values = Object.assign({},    // This should filter the properties that are undefined (not on request body)
+        slug && {slug},
+        razao_social && {razao_social},
+        email && {email}
+    );
+
+    // Using transaction because update won't be commited if
+    // logged user is trying to change someone else's empresa
+    // Also might be better to 400 everything to not give info if empresa/:id exists
+    db.transaction(trx => {
+        trx.from('empresas').where('id', '=', id).update(updated_values, '*')
+            .then(data => {
+                empresa = data[0];
+
+                if (!empresa) throw { msg:`ERROR: Empresa doesn't exist`, code: 404 }
+                if (empresa.usuario_id !== logged_user_id) throw { msg:`ERROR: Unauthorized user ${logged_user_id}`, code: 403 }
+
+                console.log('UPDATED ', empresa);
+                return res.status(200).send({
+                    success: true,
+                    data: empresa
+                });
+            })
+            .then(trx.commit)
+            .catch(err => {
+                trx.rollback(err);
+
+                console.log(err);
+
+                let code = err.code;
+                if (!code) code = 400;
+
+                return res.status(code).send({
+                    success: false,
+                    data: null
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err) // Doubling logs, but I don't know if I might miss something if I delete this
+        });
+}
+
 module.exports = {
     validateRequest,
     list,
-    create
+    create,
+    update
 }
